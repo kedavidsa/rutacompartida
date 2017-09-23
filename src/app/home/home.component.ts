54bfed0a17a116a15ffd66012538ab4153ad2be6
Sebastian Polanco <jspolancor@gmail.com>
2017-09-23 07:08:28 -0500

    Implementacion inicial de la vista de rutas

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 
-
-
 @Component({
   selector: 'home-page',
   templateUrl: './home.component.html',
@@ -15,4 +13,80 @@ export class HomePageComponent {
 
   lat2: number = 51.678418;
   lng2: number = 7.809007;
+
+  trips = [
+    'Taxi',
+    'Uber',
+    'Transmilenio',
+    'SITP',
+    'Bicicleta',
+    'Caminata'
+  ];
+
+  days = [
+    {
+      label: 'LU',
+      active: false
+    },
+    {
+      label: 'MA',
+      active: true
+    },
+    {
+      label: 'MI',
+      active: false
+    },
+    {
+      label: 'JU',
+      active: false
+    },
+    {
+      label: 'VI',
+      active: true
+    },
+    {
+      label: 'SA',
+      active: false
+    },
+    {
+      label: 'DO',
+      active: false
+    }
+  ];
+
+  routes = [
+    {
+      name: 'Oficina - Estación Alcala',
+      start: 'calle falsa 123',
+      startLat: '',
+      startLon: '',
+      end: 'calle falsa 123',
+      endLat: '',
+      endLon: ''
+    },
+    {
+      name: 'Oficina - Casa',
+      start: 'calle falsa 123',
+      startLat: '',
+      startLon: '',
+      end: 'calle falsa 123',
+      endLat: '',
+      endLon: ''
+    },
+    {
+      name: 'Casa - Oficina',
+      start: 'calle falsa 123',
+      startLat: '',
+      startLon: '',
+      end: 'calle falsa 123',
+      endLat: '',
+      endLon: ''
+    }
+  ];
+
+  getReverseGeocoding(address){
+    console.log('test');
+    return address;
+  }
+
 }
