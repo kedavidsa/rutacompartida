776cd7cbe635e4dd4b2c9e6f5238e5cafbd4cb1f
Sebastian Polanco <jspolancor@gmail.com>
2017-09-28 01:09:03 -0500

    Agrega el autocomplede de arcgis a la vista de crear ruta

--- src/app/newruta/newruta.component.ts
+++ src/app/newruta/newruta.component.ts
@@ -7,7 +7,7 @@ import {
 } from "@angular/core";
 import { FormControl, Validators } from "@angular/forms";
 import { MapsAPILoader } from "@agm/core";
-import {} from "googlemaps";
+import { } from "googlemaps";
 import { Router } from "@angular/router";
 import { Ruta, RutasService, ACTIVO } from "../rutas.service";
 import { Http } from "@angular/http";
@@ -15,6 +15,9 @@ import { ViewContainerRef } from "@angular/core";
 import _ from "lodash";
 import { ToastsManager } from "ng2-toastr/ng2-toastr";
 import { LoginService } from "../login.service";
+import { Observable } from "rxjs/Observable";
+import 'rxjs/add/operator/startWith';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: "app-newruta",
@@ -32,6 +35,7 @@ export class NewrutaComponent implements OnInit {
   rutaFormControl = new FormControl("", [Validators.required]);
   diriniFormControl = new FormControl("", [Validators.required]);
   dirdestFormControl = new FormControl("", [Validators.required]);
+  suggestions = [];
 
   days = [
     {
@@ -98,6 +102,7 @@ export class NewrutaComponent implements OnInit {
       active: false
     }
   ];
+
   ruta: Ruta = {
     key: "",
     nombre: "",
@@ -129,9 +134,11 @@ export class NewrutaComponent implements OnInit {
       walk: false
     }
   };
+
   @ViewChild("search") public searchElementRef: ElementRef;
   username: string;
   userpic: string;
+  emptySuggestions: any[];
   constructor(
     private mapsAPILoader: MapsAPILoader,
     private ngZone: NgZone,
@@ -142,8 +149,27 @@ export class NewrutaComponent implements OnInit {
     public viewContainerRef: ViewContainerRef,
     private authService: LoginService,
   ) {
+    // Listener for autocomplete
+    this.diriniFormControl.valueChanges
+    .startWith(null)
+    .subscribe(query => query ? this.autocompleteAddresses(query).then(data => { 
+      this.suggestions = [];
+      for(let i in data){
+        this.suggestions.push(data[i]);
+      }
+    }) : []);
+    // Listener for autocomplete
+    this.dirdestFormControl.valueChanges
+    .startWith(null)
+    .subscribe(query => query ? this.autocompleteAddresses(query).then(data => { 
+      this.suggestions = [];
+      for(let i in data){
+        this.suggestions.push(data[i]);
+      }
+    }) : []);
+
     this.authService.user.subscribe(user => {
-      if(user){
+      if (user) {
         this.username = user.displayName;
         this.userpic = user.photoURL;
       }
@@ -155,9 +181,9 @@ export class NewrutaComponent implements OnInit {
     });
   }
 
-  ngOnInit() {}
+  ngOnInit() { }
+
   saveAddressOnFirebase() {
-    
 
     this.ruta.init.lat = this.startCoordinates.lat;
     this.ruta.init.long = this.startCoordinates.lng;
@@ -178,6 +204,17 @@ export class NewrutaComponent implements OnInit {
     this.router.navigate(["/home"]);
   }
 
+  autocompleteAddresses(query: string) {
+    const replaced = query.replace(' ', '+');
+    const apiUrl = `http://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/suggest?text=${query}&category=&location=&distance=&f=pjson&countryCode=CO`;
+    return new Promise(resolve =>{
+      this.http.request(apiUrl)
+      .subscribe(data => {
+        resolve(data.json().suggestions)
+      })
+    });
+  }
+
   save() {
     const self = this;
     self.savingRoute = true;
@@ -186,37 +223,34 @@ export class NewrutaComponent implements OnInit {
     let startUrl = `${gapiUrl}&Address=${this.start}`;
     let endUrl = `${gapiUrl}&Address=${this.end}`;
 
-    self.http.request(startUrl).subscribe(function(startResponse) {
-      
+    self.http.request(startUrl).subscribe(function (startResponse) {
+
       const parsedStartResponse = JSON.parse(startResponse.text());
-      if(parsedStartResponse.candidates!=null&&parsedStartResponse.candidates[0]&&parsedStartResponse.candidates[0].score==100){
-        let startCandidate=parsedStartResponse.candidates[0];
-        self.startCoordinates={lat:startCandidate.location.y,lng:startCandidate.location.x}
-        
-        
-        self.http.request(endUrl).subscribe(function(endResponse) {
+      if (parsedStartResponse.candidates != null && parsedStartResponse.candidates[0] && parsedStartResponse.candidates[0].score == 100) {
+        let startCandidate = parsedStartResponse.candidates[0];
+        self.startCoordinates = { lat: startCandidate.location.y, lng: startCandidate.location.x }
+
+        self.http.request(endUrl).subscribe(function (endResponse) {
           const parsedEndResponse = JSON.parse(endResponse.text());
-          if(parsedEndResponse.candidates!=null&&parsedEndResponse.candidates[0]&&parsedEndResponse.candidates[0].score==100){
-            let endCandidate=parsedEndResponse.candidates[0];
-            self.endCoordinates={lat:endCandidate.location.y,lng:endCandidate.location.x}
+          if (parsedEndResponse.candidates != null && parsedEndResponse.candidates[0] && parsedEndResponse.candidates[0].score == 100) {
+            let endCandidate = parsedEndResponse.candidates[0];
+            self.endCoordinates = { lat: endCandidate.location.y, lng: endCandidate.location.x }
             self.saveAddressOnFirebase();
-    
+
           }
-          else{
+          else {
+            self.savingRoute = false;
             self.toastr.error("Corrige las direcciones!", "Error");
           }
-          
-    
+
         });
 
-
-
       }
-      else{
+      else {
+        self.savingRoute = false;
         self.toastr.error("Corrige las direcciones!", "Error");
       }
       
-
     });
 
     // Get start and end coordinates
@@ -250,12 +284,32 @@ export class NewrutaComponent implements OnInit {
     });*/
   }
 
-  geocodeArcgis(address:string){
-    var startUrl=`${address}`;
+  geocodeArcgis(address: string) {
+    var startUrl = `${address}`;
   }
 
   logout() {
     this.authService.logout();
   }
- 
+
+  buttonState() {
+    // Route has a name
+    const routeHasAName = this.name.length > 0;
+    // Route has first address
+    const routeHasFirstAddress = this.start.length > 0;
+    // Route has second address
+    const routeHasSecondAddress = this.end.length > 0;
+    // Route has at least one day
+    const selectedDays = _.filter(this.days, day => {
+      return day.active;
+    });
+    const hasSelectedDays = selectedDays.length > 0;
+    // Route has at list one vehicle
+    const selectedTypes = _.filter(this.types, type => {
+      return type.active;
+    });
+    const hasSelectedTypes = selectedTypes.length > 0;
+    return routeHasAName && routeHasFirstAddress && routeHasSecondAddress && hasSelectedDays && hasSelectedTypes;
+  }
+
 }
